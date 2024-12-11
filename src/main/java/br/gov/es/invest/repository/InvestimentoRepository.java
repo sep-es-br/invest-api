5c18a28550f13b00d44c8793f9b90f8990877064
Gean Guimarães Carneiro <gean.carneiro@sep.local>
2024-12-11 08:28:11 -0300

    ajustar query da listagem de investimentos

--- src/main/java/br/gov/es/invest/repository/InvestimentoRepository.java
+++ src/main/java/br/gov/es/invest/repository/InvestimentoRepository.java
@@ -11,16 +11,22 @@ import br.gov.es.invest.model.Investimento;
 public interface InvestimentoRepository extends  Neo4jRepository<Investimento, String> {
 
     @Query("MATCH\r\n" + //
-            "    (ano:Ano)<-[:EM]-(exec:ExecucaoOrcamentaria)-[rd:DELIMITA]->(conta:Investimento)<-[rc:CUSTEADO]-(obj:Objeto)<-[re:ESTIMADO]-(custo:Custo)-[:EM]->(anoCusto:Ano), \r\n" + //
-            "    (unidade)-[ri:IMPLEMENTA]->(conta)<-[ro:ORIENTA]-(plano:PlanoOrcamentario),\r\n" + //
-            "    (custo)<-[:INDICADA]-(fonte:FonteOrcamentaria)-[:VINCULA]->(exec)\r\n" + //
-            "WHERE ($exercicio IS null OR ano.ano = $exercicio OR anoCusto.ano = $exercicio) \r\n" + //
-            "    AND ($nome IS NULL OR apoc.text.clean(conta.nome) contains apoc.text.clean($nome) OR apoc.text.clean(obj.nome) contains apoc.text.clean($nome))\r\n" + //
-            "    AND ($codPO IS NULL OR elementId(plano) = $codPO)\r\n" + //
-            "    AND ($codUnidade IS NULL OR elementId(unidade) = $codUnidade)\r\n" + //
-            "    AND ($idFonte IS NULL OR elementId(fonte) = $idFonte)" + //
-            "RETURN conta, collect(rc), collect(obj), collect(re), collect(custo), collect(ri), collect(unidade), \r\n" + //
-            "    collect(rd), collect(exec), collect(ro), collect(plano) SKIP $skip LIMIT $limit")
+                "    (conta:Investimento)<-[rc:CUSTEADO]-(obj:Objeto),\r\n" + //
+                "    (unidade)-[ri:IMPLEMENTA]->(conta)<-[ro:ORIENTA]-(plano:PlanoOrcamentario)\r\n" + //
+                "WHERE ($codUnidade IS NULL OR elementId(unidade) = $codUnidade)\r\n" + //
+                "    AND ($codPO IS NULL OR elementId(plano) = $codPO)\r\n" + //
+                "    AND ($nome IS NULL OR apoc.text.clean(conta.nome) contains apoc.text.clean($nome) OR apoc.text.clean(obj.nome) contains apoc.text.clean($nome))\r\n" + //
+                "OPTIONAL MATCH (obj)<-[re:ESTIMADO]-(custo:Custo),\r\n" + //
+                "               (anoCusto:Ano)<-[:EM]-(custo)<-[:INDICADA]-(fonteCusto:FonteOrcamentaria)\r\n" + //
+                "WHERE ($exercicio IS NULL OR anoCusto.ano = $exercicio)\r\n" + //
+                "    AND ($idFonte IS NULL OR elementId(fonteCusto) = $idFonte) \r\n" + //
+                "OPTIONAL MATCH (exec:ExecucaoOrcamentaria)-[rd:DELIMITA]->(conta),\r\n" + //
+                "        (anoExec:Ano)<-[:EM]-(exec)<-[:VINCULA]-(fonteExec:FonteOrcamentaria)\r\n" + //
+                "\r\n" + //
+                "WHERE ($exercicio IS null OR anoExec.ano = $exercicio) \r\n" + //
+                "    AND ($idFonte IS NULL OR elementId(fonteExec) = $idFonte)\r\n" + //
+                "RETURN conta, collect(rc), collect(obj), collect(re), collect(custo), collect(ri), collect(unidade),\r\n" + //
+                "    collect(rd), collect(exec), collect(ro), collect(plano) SKIP $skip LIMIT $limit")
     public List<Investimento> findAllByFilter(
         String nome, String codUnidade, String codPO,
         String exercicio, String idFonte, Pageable pageable
