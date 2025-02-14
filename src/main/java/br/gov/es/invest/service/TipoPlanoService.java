2a472d71fa841552a069d59da9be71fda8a3257a
Gean Guimarães Carneiro <gean.carneiro@sep.local>
2025-02-14 15:13:52 -0300

    finalizar

--- src/main/java/br/gov/es/invest/service/TipoPlanoService.java
+++ src/main/java/br/gov/es/invest/service/TipoPlanoService.java
@@ -2,8 +2,10 @@ package br.gov.es.invest.service;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
@@ -20,6 +22,19 @@ public class TipoPlanoService {
     public List<TipoPlano> findAll(){
         
         return repository.findAll(Sort.by(Sort.Direction.ASC, "nome"));
+    }
+
+    public Optional<TipoPlano> findById(String id) {
+        return repository.findById(id);
+    }
+
+    public Optional<TipoPlano> findBySigla(String sigla) {
+
+        TipoPlano probe = new TipoPlano();
+        probe.setSigla(sigla);
+
+        return repository.findBy(Example.of(probe), q -> q.first());
+
     }
     
 
