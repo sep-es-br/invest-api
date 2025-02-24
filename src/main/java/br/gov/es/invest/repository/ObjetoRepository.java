801aef589556c10100859b29a8865cfdadfc9c04
Gean Guimarães Carneiro <gean.carneiro@sep.local>
2025-02-24 19:10:38 -0300

    corrigir contagem

--- src/main/java/br/gov/es/invest/repository/ObjetoRepository.java
+++ src/main/java/br/gov/es/invest/repository/ObjetoRepository.java
@@ -36,7 +36,7 @@ public interface ObjetoRepository extends Neo4jRepository<Objeto, String> {
                         "RETURN distinct obj, collect(rc), collect(emStatus), collect (status), collect(indicada),\r\n" + //
                         "    collect(conta), collect(orienta), collect(plano), collect(ri), collect(unidade),\r\n" + //
                         "    collect(rd), collect(execucao), collect(re), collect(custo), collect(vinculada),\r\n" + //
-                        "    collect(fonteCusto), collect(fonteExec), collect(emEtapa), collect(etapa)" + //
+                        "    collect(fonteCusto), collect(fonteExec), collect(emEtapa), collect(etapa) " + //
                 "SKIP $skip LIMIT $limit")
     public List<ObjetoTiraProjection> getAllListByFilter(Integer exercicio, String nome, List<String> idUnidade, List<String> idPo, String statusId, Pageable pageable);
 
@@ -106,20 +106,20 @@ public interface ObjetoRepository extends Neo4jRepository<Objeto, String> {
                         "    RETURN obj, rc, orienta, plano, ri, unidade, rd, execucao, emEtapa, etapa,\r\n" + //
                         "        re, custo, conta, emStatus, status, indicada, fonteCusto, vinculada, fonteExec\r\n" + //
                         "} WITH  obj, rc, orienta, plano, ri, unidade, rd, execucao, emEtapa, etapa, \r\n" + //
-                        "        re, custo, conta, emStatus, status, indicada, fonteCusto, vinculada, fonteExec\r\n" + // //
+                        "        re, custo, conta, emStatus, status, indicada, fonteCusto, vinculada, fonteExec\r\n" + //
                         "WHERE ($nome IS NULL OR apoc.text.clean(obj.nome) CONTAINS apoc.text.clean($nome))\r\n" + //
                         "    AND ($exercicio IS NULL OR custo.anoExercicio = $exercicio OR execucao.anoExercicio = $exercicio)\r\n" + //
-                        "    AND ($idUnidade IS NULL OR elementId(unidade) = $idUnidade)\r\n" + //
+                        "    AND ($idUnidade IS NULL OR elementId(unidade) IN $idUnidade)\r\n" + //
                         "    AND ($statusId IS NULL OR elementId(status) = $statusId)\r\n" + //
                         "    AND (\r\n" + //
                         "        $idPo IS NULL\r\n" + //
                         "        OR (\"S.PO\" IN $idPo AND plano IS NULL)\r\n" + //
-                        "        OR (NOT \"S.PO\" IN $idPo AND elementId(plano) IN $idPo)\r\n" + //
+                        "        OR ( NOT \"S.PO\" IN $idPo AND elementId(plano) IN $idPo)\r\n" + //
                         "        )\r\n" + //
                         "RETURN distinct obj, collect(rc), collect(emStatus), collect (status), collect(indicada),\r\n" + //
                         "    collect(conta), collect(orienta), collect(plano), collect(ri), collect(unidade),\r\n" + //
                         "    collect(rd), collect(execucao), collect(re), collect(custo), collect(vinculada),\r\n" + //
-                        "    collect(fonteCusto), collect(fonteExec), collect(emEtapa), collect(etapa)")
+                        "    collect(fonteCusto), collect(fonteExec), collect(emEtapa), collect(etapa) ")
         public List<ObjetoTiraProjection> getAllListByFilter(Integer exercicio, String nome, List<String> idUnidade, List<String> idPo, String statusId);
 
         @Query("MATCH (n:Objeto)-[:EM]->(status:Status) \r\n" + //
