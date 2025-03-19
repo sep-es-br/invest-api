827e97d34af66fff07b588b20eea1ffc8cae3a1b
Gean Guimarães Carneiro <gean.carneiro@sep.local>
2025-03-19 17:23:38 -0300

    inicio da geração da planilha

--- src/main/java/br/gov/es/invest/dto/RegistroDadoDetalhado.java
+++ src/main/java/br/gov/es/invest/dto/RegistroDadoDetalhado.java
@@ -1,11 +1,40 @@
 package br.gov.es.invest.dto;
 
+import java.util.List;
+
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
 @Setter
 @Builder
 public class RegistroDadoDetalhado {
+    private String unidadeResponsável;
+    private String codPo;
+    private String nomePo;
+    private String tipoDePlano;
+    private String microrregiao;
+    private String areaEstrategica;
+    private String contrato;
+    private String gnd;
+    private List<ValoresPorFonte> valoresPorFonte;
+}
+
+@Getter
+@Setter
+@Builder
+class ValoresPorFonte {
+    private String fonte;
+    private List<ValoresPorAno> valoresPorAno;
+}
+
+@Getter
+@Setter
+@Builder
+class ValoresPorAno {
+    private String ano;
+    private String contratado;
+    private String previsto;
     
 }
