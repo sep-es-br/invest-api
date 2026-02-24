c3b7df41f82106eee429ea20ed3cc2ef81dae9a4
Gean G. Carneiro <geancarneiro1@gmail.com>
2026-02-24 15:14:06 -0300

    ajuste do permissionamento

--- src/main/java/br/gov/es/invest/service/AutenticacaoService.java
+++ src/main/java/br/gov/es/invest/service/AutenticacaoService.java
@@ -91,9 +91,8 @@ public class AutenticacaoService {
          * 
          */
         
-        List<PapelACResponseDto> papeisPrioritarios = papeisAc.stream().filter(p -> p.Prioritario()).toList();
         
-        for(PapelACResponseDto papelAc : papeisPrioritarios.isEmpty() ? papeisAc : papeisPrioritarios ){
+        for(PapelACResponseDto papelAc :  papeisAc ){
             
             Optional<Papel> papelBanco = papelSrv.findByGuid(papelAc.Guid());
             
@@ -102,7 +101,7 @@ public class AutenticacaoService {
                     return true;
             }
             
-            if(papelAc.LotacaoGuid() != null) {
+            if(papelAc.LotacaoGuid() != null && papelAc.Prioritario()) {
                 
                 UnidadeACResponseDto setorAc = acService.getUnidadeInfoByGuid(papelAc.LotacaoGuid(), clientToken);
                 
